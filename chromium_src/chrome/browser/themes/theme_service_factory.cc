3270f8be47c5f366eacc74aeae4c947952203190
mkarolin <max@brave.com>
2020-02-05 20:26:20 -0500

    theme_service_win -> theme_helper_win.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/1160e52886b813ebd72d57f66613f6819abe1344
    
    commit 1160e52886b813ebd72d57f66613f6819abe1344
    Author: Alan Cutter <alancutter@chromium.org>
    Date:   Thu Jan 30 04:10:02 2020 +0000
    
        Decouple browser theme code from currently installed theme object
    
        This CL removes all references to ThemeService::theme_supplier_ when
        computing colors/images by moving the logic up into separate
        ThemeServiceBase class.
    
        Previously, to return a default theme value,
        ThemeService::BrowserThemeProvider used a helper class called
        DefaultScope that would swap ThemeService::theme_supplier_ with nullptr
        while it was in scope to simulate the user having no theme installed in
        order to compute default color values.
        This CL cleans up this hack by making theme_supplier an explicitly
        passed in parameter to the theme logic which has been pulled out into a
        separate ThemeHelper utility for the ThemeService such that it does
        not depend on the active theme_supplier_.
    
        This is a pure refactor with no changes to existing behaviour.
    
        This is in preparation to allow BrowserFrame Widgets to use their own
        CustomThemeSuppliers:
        https://chromium-review.googlesource.com/c/chromium/src/+/2003156
    
        Bug: 1020050

--- chromium_src/chrome/browser/themes/theme_service_factory.cc
+++ chromium_src/chrome/browser/themes/theme_service_factory.cc
@@ -1,18 +1,53 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+/* Copyright (c) 2020 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include <memory>
+
+#include "base/no_destructor.h"
 #include "build/build_config.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/themes/theme_service.h"
+#include "chrome/browser/themes/theme_service_factory.h"
 
 #if defined(OS_WIN)
-#include "brave/browser/themes/brave_theme_service_win.h"
-#undef ThemeServiceWin
-#define ThemeServiceWin BraveThemeServiceWin
-#elif !defined(USE_X11)
+#include "brave/browser/themes/brave_theme_helper_win.h"
+#else
+#include "brave/browser/themes/brave_theme_helper.h"
+#endif
+
+#if !defined(USE_X11)
+namespace {
+
+// Forward declare the original function.
+const ThemeHelper& GetThemeHelper();
+
+const ThemeHelper& GetBraveThemeHelper(Profile* profile) {
+#if defined(OS_WIN)
+  using BraveThemeHelper = BraveThemeHelperWin;
+#endif
+  // Prevent unused function error for the original function.
+  const ThemeHelper& unused = GetThemeHelper();
+  (void)unused;
+
+  // Create BraveThemeHelper and pass the profile into it.
+  static base::NoDestructor<std::unique_ptr<ThemeHelper>> theme_helper(
+      std::make_unique<BraveThemeHelper>());
+  (static_cast<BraveThemeHelper*>(theme_helper.get()->get()))
+      ->SetProfile(profile);
+  return **theme_helper;
+}
+
+}  // namespace
+
 #include "brave/browser/themes/brave_theme_service.h"
-#undef ThemeService
 #define ThemeService BraveThemeService
+#define BRAVE_THEMESERVICEFACTORY_BUILDSERVICEINSTANCEFOR \
+  GetBraveThemeHelper(static_cast<Profile*>(profile))
+#else
+#define BRAVE_THEMESERVICEFACTORY_BUILDSERVICEINSTANCEFOR \
+  GetThemeHelper()
 #endif
 
 #include "../../../../../chrome/browser/themes/theme_service_factory.cc"

180beb8509c6de43f1e1953a6a2aee6adc8d160f
NejcZdovc <nejc.zdovc@3zsistemi.si>
2020-02-10 12:30:54 +0100

    Moves media publisher info table

--- vendor/bat-native-ledger/src/bat/ledger/internal/database/database_migration.cc
+++ vendor/bat-native-ledger/src/bat/ledger/internal/database/database_migration.cc
@@ -7,6 +7,7 @@
 #include <utility>
 
 #include "bat/ledger/internal/database/database_activity_info.h"
+#include "bat/ledger/internal/database/database_media_publisher_info.h"
 #include "bat/ledger/internal/database/database_migration.h"
 #include "bat/ledger/internal/database/database_pending_contribution.h"
 #include "bat/ledger/internal/database/database_publisher_info.h"
@@ -22,6 +23,7 @@ namespace braveledger_database {
 DatabaseMigration::DatabaseMigration(bat_ledger::LedgerImpl* ledger) :
     ledger_(ledger) {
   activity_info_ = std::make_unique<DatabaseActivityInfo>(ledger_);
+  media_publisher_info_ = std::make_unique<DatabaseMediaPublisherInfo>(ledger_);
   pending_contribution_ =
       std::make_unique<DatabasePendingContribution>(ledger_);
   publisher_info_ = std::make_unique<DatabasePublisherInfo>(ledger_);
@@ -75,6 +77,10 @@ bool DatabaseMigration::Migrate(
     ledger::DBTransaction* transaction,
     const int target) {
   if (!activity_info_->Migrate(transaction, target)) {
+    return false;
+  }
+
+  if (!media_publisher_info_->Migrate(transaction, target)) {
     return false;
   }
 
